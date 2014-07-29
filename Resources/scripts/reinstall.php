6e271d88993f0e564a1436b1483bb88bce3aa4be
ngodfraind <nicolas.godfraind@gmail.com>
2014-07-29 17:10:28 +0200

    Moving demo fixtures from corebundle.

--- Resources/scripts/reinstall.php
+++ Resources/scripts/reinstall.php
@@ -6,6 +6,9 @@ $console = 'php ' . $rootPath . '/app/console ';
 system($console . 'claroline:demo:refresh');
 
 //if you want to load some fixtures, do it here.
-system($console . 'claroline:fixture:demo');
+system($console . 'claroline:demo:load --env=prod');
+
+//removing the maintenance mode.
+@unlink($rootPath . '/app/config/.update');
 
 system('chmod -R 777 ' . $rootPath . '/app/cache/*');
